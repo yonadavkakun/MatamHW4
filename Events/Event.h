068f2c42f86dec193c60d21d5c885aa17f85ccbd
yonadavkakun <yonadav4321@gmail.com>
2024-08-08 15:50:44 +0300

    open Event.h

--- Events/Event.h
+++ Events/Event.h
@@ -1,14 +1,56 @@
 
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "../Players/Player.h"
 
 class Event {
+    string name;
+
 public:
     /**
      * Gets the description of the event
      *
      * @return - the description of the event
     */
-    string getDescription() const;
+    virtual string getDescription() const =0;
+
+    virtual void applyEvent() =0;
+};
+
+class SolarEclipse {
+public:
+    SolarEclipse();
+};
+
+class PotionsMerchant {
+public:
+    PotionsMerchant();
+};
+
+class Snail {
+public:
+    Snail();
+};
+
+class Slime {
+public:
+    Slime();
+};
+
+class Balrog {
+    int extraPoints;
+
+public:
+    Balrog();
+};
+
+class Pack {
+    int packSize;
+    std::vector<std::unique_ptr<Event> > pack;
+
+public:
+    Pack(int packSize);
 };
