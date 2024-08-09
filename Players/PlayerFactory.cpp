135f4e7f02ffdef0311d9cbad5ee1da08d08e989
yonadavkakun <yonadav4321@gmail.com>
2024-08-09 13:48:50 +0300

    update PlayerFactory

--- Players/PlayerFactory.cpp
+++ Players/PlayerFactory.cpp
@@ -5,21 +5,24 @@
 #include "PlayersJobs.h"
 
 
-std::unique_ptr<Player> PlayerFactory::createPlayer(const std::string &playerName,
-                const std::string &playerJob, const std::string &playerCharacter) {
-    switch (playerName) {
-        case "Warrior":
-            return std::make_unique<Warrior>(playerName, playerJob, playerCharacter);
-            break;
-        case "Archer":
-            return std::make_unique<Archer>(playerName, playerJob, playerCharacter);
-            break;
-        case "Magician":
-            return std::make_unique<Magician>(playerName, playerJob, playerCharacter);
-            break;
-        default:
-            //error
-            return nullptr;
-            break;
+std::shared_ptr<Player> PlayerFactory::createPlayer(const std::string &playerName,
+                                                    const std::string &playerJob, const std::string &playerCharacter) {
+    std::shared_ptr<Character> character;
+
+    if (playerCharacter == "Responsible") {
+        character = std::make_shared<Responsible>(playerCharacter);
+    } else if (playerCharacter == "RiskTaking") {
+        character = std::make_shared<RiskTaking>(playerCharacter);
+    }
+
+    if (playerJob == "Warrior") {
+        return std::make_shared<Warrior>(playerName, character);
+    } else if (playerJob == "Archer") {
+        return std::make_shared<Archer>(playerName, character);
+    } else if (playerJob == "Magician") {
+        return std::make_shared<Magician>(playerName, character);
+    } else {
+        // error
+        return nullptr;
     }
 }
