61eb6348fb893be8cb3f7789b3b4409665fcc2d4
yonadavkakun <yonadav4321@gmail.com>
2024-08-12 14:00:13 +0300

    open Event and Monster Factory

--- Events/EventFactory.h
+++ Events/EventFactory.h
@@ -1,14 +1,31 @@
 #pragma once
-
-
-#include "Event.h"
 #include <map>
+#include <memory>
+#include <functional>
+#include <stdexcept>
 #include <string>
+#include "Event.h"
+#include "MonsterFactory.h"
+using std::string;
 
 
-//EventFactory
 class EventFactory {
-    //std::map<std::string, std::unique_ptr<Event>()> eventsMap;
 public:
-    static std::shared_ptr<Event> createEvent(const std::string &eventType);
+    using EventCreator = std::function<std::shared_ptr<Event>()>;
+
+    static std::shared_ptr<Event> createEvent(const std::string &eventType) {
+        const static std::map<std::string, EventCreator> EventMap = {
+            {string("SolarEclipse"), [&]() { return std::make_shared<SolarEclipse>(eventType); }},
+            {string("PotionsMerchant"), [&]() { return std::make_shared<PotionsMerchant>(eventType); }},
+            {string("Snail"), [&]() { return MonsterFactory::createMonster(eventType); }},
+            {string("Slime"), [&]() { return MonsterFactory::createMonster(eventType); }},
+            {string("Balrog"), [&]() { return MonsterFactory::createMonster(eventType); }},
+        };
+
+        auto it = EventMap.find(eventType);
+        if (it != EventMap.end()) {
+            return it->second();
+        }
+        throw std::runtime_error("Invalid Event type");
+    }
 };
---
+++ Events/MonsterFactory.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <functional>
+#include <stdexcept>
+#include <string>
+#include "Event.h"
+using std::string;
+
+
+class MonsterFactory {
+public:
+    using MonsterCreator = std::function<std::shared_ptr<Monster>()>;
+
+    static std::shared_ptr<Monster> createMonster(const std::string &monsterType) {
+        const static std::map<std::string, MonsterCreator> MonsterMap = {
+            {string("Snail"), [&]() { return std::make_shared<Snail>(monsterType); }},
+            {string("Slime"), [&]() { return std::make_shared<Slime>(monsterType); }},
+            {string("Balrog"), [&]() { return std::make_shared<Balrog>(monsterType); }},
+        };
+
+        auto it = MonsterMap.find(monsterType);
+        if (it != MonsterMap.end()) {
+            return it->second();
+        }
+        throw std::runtime_error("Invalid Monster type");
+    }
+};
