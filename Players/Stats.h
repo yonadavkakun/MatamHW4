94f888b863ea41d1ea2367c72d12800985fe7742
yonadavkakun <yonadav4321@gmail.com>
2024-08-12 11:48:48 +0300

    open Stats.h

---
+++ Players/Stats.h
@@ -0,0 +1,88 @@
+#pragma once
+
+
+class Stats {
+ int level;
+ int force;
+ int currHealthPoints;
+ int maxHealthPoints;
+ int coins;
+
+public:
+ /**
+  * Stats c'tor
+  *
+  * by defult: level=1, force=5, currHP=maxHP=100, coins=10.
+ */
+ Stats();
+
+ /**
+  * Gets the current level of the player
+  *
+  * @return - level of the player
+ */
+ int getLevel() const;
+
+ /**
+  * sets the current level of the player
+  *
+  * @return - level of the player
+ */
+ void setLevel();
+
+ /**
+  * Gets the of force the player has
+  *
+  * @return - force points of the player
+ */
+ int getForce() const;
+
+ /**
+     * sets the of force the player has
+     *
+     * @return - force points of the player
+    */
+ void setForce();
+
+ /**
+  * Gets the amount of health points the player currently has
+  *
+  * @return - health points of the player
+ */
+ int getHealthPoints() const;
+
+ /**
+ * sets the amount of health points the player currently has
+ *
+ * @return - health points of the player
+*/
+ void setHealthPoints();
+
+ /**
+  * Gets the Max health points of the player
+  *
+  * @return - max health points of the player
+ */
+ int getMaxHealthPoints() const;
+
+ /**
+ * sets the Max health points of the player
+ *
+ * @return - max health points of the player
+*/
+ void setMaxHealthPoints() const;
+
+ /**
+  * Gets the amount of coins the player has
+  *
+  * @return - coins of the player
+ */
+ int getCoins() const;
+
+ /**
+ * sets the amount of coins the player has
+ *
+ * @return - coins of the player
+*/
+ void setCoins();
+};
