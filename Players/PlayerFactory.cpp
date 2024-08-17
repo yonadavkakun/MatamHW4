62486b18851374f8f605203c6c7373e8c4576592
yonadavkakun <yonadav4321@gmail.com>
2024-08-17 23:02:18 +0300

    updating playerName checking

--- Players/PlayerFactory.cpp
+++ Players/PlayerFactory.cpp
@@ -3,13 +3,7 @@
 std::unique_ptr<Player> PlayerFactory::createPlayer(std::istream &playersStream) {
     string playerName, playerJob, playerCharacter;
     playersStream >> playerName >> playerJob >> playerCharacter;
-    int nameSize = 0;
-    for (char c: playerName) {
-        if (!std::isalpha(c)) {
-            throw std::runtime_error("Invalid Players File");
-        }
-        nameSize++;
-    }
+    int nameSize = playerName.size();
     if (nameSize > 15 || nameSize < 3) {
         throw std::runtime_error("Invalid Players File");
     }
